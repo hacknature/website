40c0768afd57b143012c0ba65d83e1391ca48154
Erik Langhout <erik.langhout@elonisas.nl>
2025-02-24 00:46:37 +0100

    Testing workflow actions

--- public/manifest.js
+++ public/manifest.js
@@ -5,13 +5,14 @@
     "src": "/img/icons/favicon-16x16.png",
     "sizes": "16x16",
     "type": "image/png"
-  }, {
+  },
+  {
     "src": "/img/icons/favicon-32x32.png",
     "sizes": "32x32",
     "type": "image/png"
 
   }],
-  "start_url": "/index.html",
+  "start_url": "/",
   "display": "standalone",
   "orientation" : "portrait",
   "background_color": "#000000",
